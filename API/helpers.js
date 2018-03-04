64b924cd30e48a7a569dc14a3b27b6a710e8e355
Alejandro García <alejogs4@gmail.com>
2018-03-04 00:19:40 -0500

    Agrega datos para ser usados en toda la API

---
+++ API/helpers.js
@@ -0,0 +1,14 @@
+const OK = 200
+const CREATED = 201
+const SERVER_ERROR = 500
+const NOT_FOUND = 404
+
+const c = console.log
+
+module.exports = {
+    OK,
+    CREATED,
+    SERVER_ERROR,
+    NOT_FOUND,
+    c
+}
