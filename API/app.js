0ec68e29111aa0e53a30077dd8242d6eb8d67ea6
Alejandro García <alejogs4@gmail.com>
2018-03-04 00:19:54 -0500

    Agrega rutas y accesos

--- API/app.js
+++ API/app.js
@@ -2,7 +2,20 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
-app.use(bodyParser.json)
+//Routes
+const parking = require('./routes/parking_routes')
+
+
+const cross = (req, res, next) => {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+}
+
+app.use(cross)
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended : false }))
+app.use(parking)
 
 module.exports = app
